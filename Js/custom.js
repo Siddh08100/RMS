04669e6b6dac4d1a9147c9be83d30ab55bbc1b12
Siddh08100 <tatvasoft.pcq113@outlook.com>
2025-01-30 18:47:14 +0530

    Navigation and Menu page

--- Js/custom.js
+++ Js/custom.js
@@ -1,88 +1,100 @@
-// const togglePassword = document.querySelector('#togglePassword');
-// const password = document.querySelector('#password');
+document.addEventListener("DOMContentLoaded", function () {
+  loadComponent("sidebar", "sidebar.html");
+  loadComponent("MainContent", "menu.html");// change here main dashboard
+  loadComponent("navbar", "navbar.html");
+});
 
-// togglePassword.addEventListener('click', () => {
-//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-//     password.setAttribute('type', type);
-//     togglePassword.classList.toggle('bi-eye');
-// });
+function loadComponent(id, url) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+function loadContent(page) {
+  fetch(page)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("MainContent").innerHTML = data;
+    });
+}
 
 function validateEmailInput() {
-    const emailInput = document.getElementById('email').value;
-    const feedbackElement =document.querySelector('#email-error-txt');
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-    if (emailRegex.test(emailInput)) {
-      feedbackElement.textContent = ""; 
-    } else {
-      feedbackElement.textContent = "Please enter a valid email address.";
-    }
+  const emailInput = document.getElementById("email").value;
+  const feedbackElement = document.querySelector("#email-error-txt");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(emailInput)) {
+    feedbackElement.textContent = "";
+  } else {
+    feedbackElement.textContent = "Please enter a valid email address.";
+  }
+}
+
+function Try() {
+  const togglePassword = document.getElementById("togglePassword");
+  const password = document.getElementById("password");
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  console.log(togglePassword);
+  console.log(password);
+
+  if (type === "password") {
+    togglePassword.setAttribute("class", "fa fa-eye-slash");
+  } else {
+    togglePassword.setAttribute("class", "fa fa-eye");
   }
 
-  function Try(){
-    const togglePassword = document.getElementById('togglePassword')
-    const password = document.getElementById('password')
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    console.log(togglePassword);
-    console.log(password);
-    
-    if(type === 'password'){
-        togglePassword.setAttribute('class','fa fa-eye-slash')
-    }else{
-        togglePassword.setAttribute('class','fa fa-eye')
-    }
+  password.setAttribute("type", type);
+}
 
-    password.setAttribute('type', type);
+function newPassword() {
+  const togglePassword = document.getElementById("newPasswordToggle");
+  const password = document.getElementById("newPassword");
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
 
+  if (type === "password") {
+    togglePassword.setAttribute("class", "fa fa-eye-slash");
+  } else {
+    togglePassword.setAttribute("class", "fa fa-eye");
   }
 
-  function newPassword(){
-    const togglePassword = document.getElementById('newPasswordToggle')
-    const password = document.getElementById('newPassword')
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute("type", type);
+}
 
-    if(type === 'password'){
-        togglePassword.setAttribute('class','fa fa-eye-slash')
-    }else{
-        togglePassword.setAttribute('class','fa fa-eye')
-    }
+function confirmNewPassword() {
+  const togglePassword = document.getElementById("confirmNewPasswordToggle");
+  const password = document.getElementById("confirmNewPassword");
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
 
-    password.setAttribute('type', type);
-
-  }
-  
-  function confirmNewPassword(){
-    const togglePassword = document.getElementById('confirmNewPasswordToggle')
-    const password = document.getElementById('confirmNewPassword')
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-
-    if(type === 'password'){
-        togglePassword.setAttribute('class','fa fa-eye-slash')
-    }else{
-        togglePassword.setAttribute('class','fa fa-eye')
-    }
-
-    password.setAttribute('type', type);
-
+  if (type === "password") {
+    togglePassword.setAttribute("class", "fa fa-eye-slash");
+  } else {
+    togglePassword.setAttribute("class", "fa fa-eye");
   }
 
-  function validateAll() {
-    const emailInput = document.getElementById('email').value;
-    const emailError =document.getElementById('email-error-txt');
-    
-    const passInput = document.getElementById('password').value;
-    const passError =document.getElementById('pass-error-txt');
+  password.setAttribute("type", type);
+}
 
-    if (passInput !== "") {
-        passError.innerHTML = ""; 
-      } else {
-        passError.innerHTML = "password is required";
-      }
-  
-    if (emailInput !== "") {
-        emailError.innerHTML = ""; 
-    } else {
-        emailError.innerHTML = "Email is required";
-    }
+function validateAll() {
+  const emailInput = document.getElementById("email").value;
+  const emailError = document.getElementById("email-error-txt");
 
+  const passInput = document.getElementById("password").value;
+  const passError = document.getElementById("pass-error-txt");
+
+  if (passInput !== "") {
+    passError.innerHTML = "";
+  } else {
+    passError.innerHTML = "password is required";
   }
+
+  if (emailInput !== "") {
+    emailError.innerHTML = "";
+  } else {
+    emailError.innerHTML = "Email is required";
+  }
+}
